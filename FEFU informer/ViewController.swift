47692239b8955697aa6937805d3dc91cb143a615
Ilia <musicguy675@gmail.com>
2015-10-26 15:42:04 +1000

    Initial Commit

---
+++ FEFU informer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FEFU informer
+//
+//  Created by Илья on 26.10.15.
+//  Copyright © 2015 AwakeGD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
