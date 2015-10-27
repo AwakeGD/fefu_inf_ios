900cc7349561f9dad1db2baad458620d6d4ed291
Ilia <musicguy675@gmail.com>
2015-10-27 19:51:35 +1000

    now build for iPhone 5s

---
+++ Pods/Target Support Files/Pods-FEFU informer/Pods-FEFU informer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FEFU_informer : NSObject
+@end
+@implementation PodsDummy_Pods_FEFU_informer
+@end
---
+++ Pods/Target Support Files/Pods-FEFU informer/Pods-FEFU informer-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FEFU_informerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FEFU_informerVersionString[];
+
