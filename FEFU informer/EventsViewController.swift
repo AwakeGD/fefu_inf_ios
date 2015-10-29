d4a2df5265fa673eb1e7833cd599562ced809559
Ilia <musicguy675@gmail.com>
2015-10-29 11:21:24 +1000

    add VC for details

--- FEFU informer/EventsViewController.swift
+++ FEFU informer/EventsViewController.swift
@@ -11,4 +11,6 @@ import UIKit
 class EventsViewController: UICollectionViewController{
     
     @IBOutlet var eventCollectionView: UICollectionView!
+    
+    
 }
--- FEFU informer/ViewController.swift
+++ FEFU informer/ViewController.swift
@@ -14,14 +14,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        //let fileURL = NSBundle.mainBundle().URLForResource("image", withExtension: "png")
-        
-        //Alamofire.upload(.POST, "http://31.131.24.188:8080/registerUsers", file: fileURL!)
-        
-        //Alamofire.request(.POST, "http://31.131.24.188:8080/registerUsers", parameters: parameters, encoding: .JSON)
-
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
